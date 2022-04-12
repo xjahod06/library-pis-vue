b1b43e36dc6d91efbc3f34407c8056ef26dddb71
xjahod06 <jahoda.vojta@gmail.com>
2022-04-12 09:43:39 +0200

    vue2 is da way

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# library-pis
+# test-projekt
 
 ## Project setup
 ```
--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,60 @@
-
 <template>
-  <NavBar/>
-  <button @click="count++">Count is: {{ count }}</button>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+      <p>Using stand-alone:<p/>
+      <div class="mb-4">
+        <b-avatar></b-avatar>
+        <b-avatar variant="primary" text="BV"></b-avatar>
+        <b-avatar variant="info" src="https://placekitten.com/300/300"></b-avatar>
+        <b-avatar variant="success" icon="people-fill"></b-avatar>
+      </div>
+      <p>Using in components (list group) example:<p/>
+      <b-list-group style="max-width: 300px;">
+        <b-list-group-item class="d-flex align-items-center">
+          <b-avatar class="mr-3"></b-avatar>
+          <span class="mr-auto">J. Circlehead</span>
+          <b-badge>5</b-badge>
+        </b-list-group-item>
+        <b-list-group-item class="d-flex align-items-center">
+          <b-avatar variant="primary" text="BV" class="mr-3"></b-avatar>
+          <span class="mr-auto">BootstrapVue</span>
+          <b-badge>12</b-badge>
+        </b-list-group-item>
+        <b-list-group-item class="d-flex align-items-center">
+          <b-avatar variant="info" src="https://placekitten.com/300/300" class="mr-3"></b-avatar>
+          <span class="mr-auto">Super Kitty</span>
+          <b-badge>9</b-badge>
+        </b-list-group-item>
+        <b-list-group-item class="d-flex align-items-center">
+          <b-avatar variant="success" icon="people-fill" class="mr-3"></b-avatar>
+          <span class="mr-auto">ACME group</span>
+          <b-badge>7</b-badge>
+        </b-list-group-item>
+      </b-list-group>
+    </div>
+  </div>
 </template>
 
 <script>
-import NavBar from "@/components/NavBar";
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  data() {
-    return {
-      count: 0
-    }
-  },
   name: 'App',
   components: {
-    NavBar
+    HelloWorld
   }
 }
 </script>
 
 <style>
 #app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
   color: #2c3e50;
-  background-color: #ebebeb;
+  margin-top: 60px;
 }
 </style>
