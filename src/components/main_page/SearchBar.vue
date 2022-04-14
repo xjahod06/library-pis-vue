6f37b523be919783d99fa9c8e32bfa2df7eb958a
Magdaléna Ondrušková <maggie.ondruskova@gmail.com>
2022-04-14 10:01:29 +0200

    search button fix

--- src/components/main_page/SearchBar.vue
+++ src/components/main_page/SearchBar.vue
@@ -3,8 +3,8 @@
       <b-container id="mynavbar">
         <!--    <b-nav-form >-->
         <b-row no-gutters>
-          <b-col cols="11" ><b-form-input  placeholder="Search"></b-form-input></b-col>
-          <b-col cols="1"><b-button  variant="primary" type="submit">Search</b-button></b-col>
+          <b-col  cols="11" ><b-form-input style="width: 100%" placeholder="Search"></b-form-input></b-col>
+          <b-col  cols="1"><b-button style="width: 100%" variant="primary" type="submit">Search</b-button></b-col>
         </b-row>
         <!--    </b-nav-form>-->
       </b-container>
