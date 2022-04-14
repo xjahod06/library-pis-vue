2bf1a29267d34afb710e02da965bface4ced1cb5
Radoslav Elias <radoslave0@gmail.com>
2022-04-14 13:08:11 +0200

    genre WIP

---
+++ src/pages/Genre.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="Genre">
+    <NavBar></NavBar>
+    <MainSection name="Books"></MainSection>
+    <MainSection name="Authors"></MainSection>
+    <MyFooter></MyFooter>
+  </div>
+</template>
+
+<script>
+
+import NavBar from "@/components/main_page/NavBar";
+import MyFooter from "@/components/main_page/MyFooter" ;
+import MainSection from "@/components/main_page/MainSection";
+
+export default {
+  name: "Genre.vue",
+  components: {
+    NavBar,
+    MyFooter,
+    MainSection
+  }
+}
+</script>
+
+<style scoped>
+@import "../assets/styles/main.css";
+</style>
