4a53551b3d2d017c8ffefec57d49c353dcaceed5
xjahod06 <jahoda.vojta@gmail.com>
2022-04-16 14:15:19 +0200

    FIX: fixed PUT & DELETE methods

--- src/services/ApiConnect.js
+++ src/services/ApiConnect.js
@@ -4,13 +4,6 @@ import serviceConfig from "@/services/serviceConfig";
 
 export default axios.create({
     baseURL: serviceConfig.hostname,
-    timeout: 5000,
-
-    headers: {
-        'X-Auth-Token': 'f2b6637ddf355a476918940289c0be016a4fe99e3b69c83d',
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': 'http://localhost:8080',
-        'Access-Control-Allow-Credentials': 'true'
-    }
+    timeout: 5000
 
 })
