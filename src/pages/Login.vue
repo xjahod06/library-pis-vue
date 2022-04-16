aa29021012a2d483ea3dac79907c9d6b2b45b1e4
MayBee <xbedna72@stud.fit.vutb.cz>
2022-04-16 17:03:59 +0200

    Login (not connected to backend yet)

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,7 +1,22 @@
 <template>
   <div id="login">
     <NavBar></NavBar>
-    
+    <b-container>
+      <div>
+        <form @submit.prevent="submit">
+          <div>
+            <label for="username">Username:</label>
+            <input type="text" name="username" v-model="form.username">
+          </div>
+          <div>
+            <label for="password">Password:</label>
+            <input type="text" name="password" v-model="form.password">
+          </div>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+      <p v-if="showError" id="error">Username already exists</p>
+    </b-container>
     <MyFooter></MyFooter>
   </div>
 </template>
@@ -17,6 +32,31 @@ export default {
     NavBar,
     MyFooter
   },
-  
+  data() {
+    return {
+    form: {
+      username: "",
+      pasword: "",
+    },
+    showError: false
+  };
+  },
+  methods: {
+    ...mapActions(["Login"]),
+    async submit() {
+      const User = new FormData();
+      User.append("username", this.form.username);
+      User.append("password", this.form.password);
+      try{
+        let res = await this.LoIn(User);
+        this.$router.push('/readers/:'+ res.data);
+        console.log(res);
+        this.showError = false;
+      } 
+      catch(err){
+        this.showError = true;
+      }
+    }
+  }
 }
 </script>
