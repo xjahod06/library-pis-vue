b007d7d1585043700d643bc4e6f203d7b303f8c2
MayBee <xbedna72@stud.fit.vutb.cz>
2022-04-16 12:12:03 +0200

    deleted stupid things

--- src/router/index.js
+++ src/router/index.js
@@ -37,19 +37,6 @@ const Routes = [
 
 const router = new VueRouter({
   Routes,
-  base: process.env.BASE_URL,
-});
-
-router.beforeEach((to, from, next) => {
-  if (to.matched.some((record) => record.meta.guest)) {
-    if (store.getters.isAuthenticated) {
-      next("/posts");
-      return;
-    }
-    next();
-  } else {
-    next();
-  }
 });
 
 export default router
