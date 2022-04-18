85bf6359073bffabce160f62f0874d3aa285c342
Magdaléna Ondrušková <maggie.ondruskova@gmail.com>
2022-04-18 17:37:12 +0200

    search implemented

--- src/components/main_page/SearchBar.vue
+++ src/components/main_page/SearchBar.vue
@@ -1,18 +1,29 @@
 <template>
-
       <b-container id="mynavbar">
-        <!--    <b-nav-form >-->
         <b-row no-gutters>
-          <b-col  cols="11" ><b-form-input style="width: 100%" placeholder="Search"></b-form-input></b-col>
-          <b-col  cols="1"><b-button style="width: 100%" variant="primary" type="submit">Search</b-button></b-col>
+          <b-col  cols="11" ><b-form-input style="width: 100%"
+                                           @input="sendToNavbar"
+                                           v-model="search_input"
+                                           placeholder="Search"></b-form-input></b-col>
+          <b-col  cols="1"><b-button style="width: 100%"
+                                     variant="primary"
+                                     type="submit">Search</b-button></b-col>
         </b-row>
-        <!--    </b-nav-form>-->
       </b-container>
-
 </template>
 
 <script>
 export default {
   name: 'SearchBar',
+  data() {
+    return {
+    search_input: ''
+    }
+  },
+  methods :{
+    sendToNavbar(){
+      this.$emit('event', this.search_input);
+    }
+  }
 }
 </script>
