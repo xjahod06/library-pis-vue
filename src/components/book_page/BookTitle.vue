7eb80240d8fd02ba4771cfc3d183558ab3a12d29
Magdaléna Ondrušková <maggie.ondruskova@gmail.com>
2022-04-20 15:37:52 +0200

    modal for reservation/borrowing

--- src/components/book_page/BookTitle.vue
+++ src/components/book_page/BookTitle.vue
@@ -7,10 +7,56 @@
     </b-row>
     <b-row class="text-left">
       <b-col offset-md="2">
-        <b-button @click="makeReservation" variant="info" class="mr-2"> Reserve </b-button>
-        <b-button v-if="hasElectronicCopy" @click="borrowEcopy" variant="success">Borrow electronic copy</b-button>
+        <b-button @click="showModalReservation" variant="info" class="mr-2"> Reserve </b-button>
+        <b-button v-if="hasElectronicCopy" @click="showModalBorrow" variant="success">Borrow electronic copy</b-button>
       </b-col>
     </b-row>
+
+    <!--MODAL RESERVATION--->
+    <b-modal ref="new-reservation" hide-footer title="Create new reservation" @hidden="onHidden">
+      <div class="d-block text-center">
+        <b-alert class="mt-2" v-model="showDismissibleAlert" variant="success" dismissible>
+          {{ alertMessage }}</b-alert>
+        <b-alert class="mt-2" v-model="showDismissibleAlertError" variant="danger" dismissible>
+          {{alertMessage}}</b-alert>
+        <b-form-datepicker id="example-datepicker"
+                           v-model="dateFrom"
+                           placeholder="Choose new date"
+                           class="mb-2">
+        </b-form-datepicker>
+        <b-form-datepicker id="example-datepicker"
+                           v-model="dateTo"
+                           placeholder="Choose new date"
+                           class="mb-2">
+        </b-form-datepicker>
+      </div>
+      <b-button class="mt-2" variant="outline-primary" block @click="makeReservation">Save</b-button>
+    </b-modal>
+
+    <!--MODAL ELECTRONIC BORROWING --->
+    <b-modal ref="new-borrow" hide-footer title="Borrow electronic copy" @hidden="onHidden">
+      <div class="d-block text-center">
+        <b-alert class="mt-2" v-model="showDismissibleAlert" variant="success" dismissible>
+          {{ alertMessage }}</b-alert>
+        <b-alert class="mt-2" v-model="showDismissibleAlertError" variant="danger" dismissible>
+          {{alertMessage}}</b-alert>
+
+        <b-form-datepicker id="example-datepicker"
+                           v-model="dateFrom"
+                           placeholder="Choose new date"
+                           class="mb-2">
+        </b-form-datepicker>
+
+        <b-form-datepicker id="example-datepicker"
+                           v-model="dateTo"
+                           placeholder="Choose new date"
+                           class="mb-2">
+        </b-form-datepicker>
+
+      </div>
+      <b-button class="mt-2" variant="outline-primary" block @click="makeReservation">Save</b-button>
+    </b-modal>
+
     <b-row align-h="center" class="mt-3">
       <b-col cols="4" class="text-right text-dark h5">
         Format
@@ -57,8 +103,27 @@ export default {
     pages: Number,
     hasElectronicCopy: Boolean
   },
+  data (){
+    return {
+      showDismissibleAlert: false,
+      showDismissibleAlertError: false,
+      alertMessage: '',
+      dateFrom: new Date(),
+      dateTo: new Date()
+    }
+  },
   methods: {
+    showModalReservation() {
+      this.$refs['new-reservation'].show()
+    },
+    showModalBorrow() {
+      this.$refs['new-borrow'].show()
+    },
     makeReservation(){
+      let id = this.$route.params.id;
+      let reservation = {}
+      reservation.id = id;
+
       console.log("vytvaram rezervaciu");
     },
     borrowEcopy(){
