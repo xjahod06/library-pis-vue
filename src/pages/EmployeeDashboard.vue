c6e7e9d81c38e5bd205186c96d053f17c701c0bd
xjahod06 <jahoda.vojta@gmail.com>
2022-04-21 09:24:34 +0200

    redesign

--- src/pages/EmployeeDashboard.vue
+++ src/pages/EmployeeDashboard.vue
@@ -2,87 +2,116 @@
   <div>
     <NavBar></NavBar>
     <b-container class="bv-example-row">
-      <b-row>
-        <b-col><router-link to="edit_titles"><b-card
-            text-variant="white"
-            style="height: 250px; width: 250px; margin: 20px; background: #24433e;"
-            class="text-light">
-          <b-card-title
-            style="text-align: center; background: #24433e;top: 50%">
-            Edit Titles
-          </b-card-title>
-          <b-icon icon="book"
-                  class="h1"
-                  style="width: 120px; height: 120px">
-          </b-icon>
-        </b-card></router-link></b-col>
-        <b-col><router-link to="/edit_authors"><b-card
-            text-variant="white"
-            style="height: 250px; width: 250px; margin: 20px; background: #24433e;"
-            class="text-center">
-          <b-card-title
-              style="text-align: center; background: #24433e;top: 50%">
-            Edit Authors
-          </b-card-title>
-          <b-icon icon="person-badge"
-                  class="h1"
-                  style="width: 120px; height: 120px">
-          </b-icon>
-        </b-card></router-link></b-col>
-        <b-col><router-link to="/edit_borrowings"><b-card
-            text-variant="white"
-            style="height: 250px; width: 250px; margin: 20px; background: #24433e;"
-            class="text-center">
-          <b-card-title
-              style="text-align: center; background: #24433e;top: 50%">
-            Edit Borrowings
-          </b-card-title>
-          <b-icon icon="bookmark"
-                  class="h1"
-                  style="width: 120px; height: 120px">
-          </b-icon>
-        </b-card></router-link></b-col>
+      <b-row align-h="center">
+        <b-col cols="3">
+          <router-link to="/edit_titles" class="router-link-dashboard">
+            <b-card
+              text-variant="white"
+              class="pb-4"
+              >
+            <b-card-title
+              class="text-center text-light"
+            >
+              Edit Titles
+            </b-card-title>
+            <b-icon icon="book"
+                    class="h1"
+                    style="width: 120px; height: 120px">
+            </b-icon>
+          </b-card>
+        </router-link>
+        </b-col>
+        <b-col cols="3">
+          <router-link to="/edit_authors" class="router-link-dashboard">
+            <b-card
+              text-variant="white"
+              class="pb-4"
+            >
+              <b-card-title
+                  class="text-center text-light"
+              >
+                Edit Authors
+              </b-card-title>
+              <b-icon icon="person-badge"
+                      class="h1"
+                      style="width: 120px; height: 120px">
+              </b-icon>
+            </b-card>
+          </router-link>
+        </b-col>
+        <b-col cols="3">
+          <router-link to="/edit_borrowings" class="router-link-dashboard">
+            <b-card
+                text-variant="white"
+                class="pb-4"
+            >
+              <b-card-title
+                  class="text-center text-light"
+              >
+                Edit Borrowings
+              </b-card-title>
+              <b-icon icon="bookmark"
+                      class="h1"
+                      style="width: 120px; height: 120px">
+              </b-icon>
+            </b-card>
+          </router-link>
+        </b-col>
       </b-row>
-      <b-row>
-        <b-col><router-link to="/edit_fines"><b-card
-            text-variant="white"
-            style="height: 250px; width: 250px; margin: 20px; background: #24433e;"
-            class="text-center">
-          <b-card-title
-              style="text-align: center; background: #24433e;top: 50%">
-            Edit Fines
-          </b-card-title>
-          <b-icon icon="cash-stack"
-                  class="h1"
-                  style="width: 120px; height: 120px">
-          </b-icon>
-        </b-card></router-link></b-col>
-        <b-col><router-link to="/edit_reservations"><b-card
-            text-variant="white"
-            style="height: 250px; width: 250px; margin: 20px; background: #24433e;"
-            class="text-light">
-          <b-card-title
-              style="text-align: center; background: #24433e;top: 50%">
-            Edit Reservations
-          </b-card-title>
-          <b-icon icon="journal-bookmark-fill"
-                  class="h1"
-                  style="width: 120px; height: 120px">
-          </b-icon>
-        </b-card></router-link></b-col>
-        <b-col><router-link to="/edit_readers"><b-card
-            text-variant="white"
-            style="height: 250px; width: 250px; margin: 20px; background: #24433e;"
-            class="text-center">
-          <b-card-title
-              style="text-align: center; background: #24433e;top: 50%">
-            Edit Readers
-          </b-card-title>
-          <b-icon icon="person"
-                  class="h1"
-                  style="width: 120px; height: 120px">
-          </b-icon>
-        </b-card></router-link></b-col>
+      <b-row align-h="center" class="mt-4">
+        <b-col cols="3">
+          <router-link to="/edit_fines" class="router-link-dashboard">
+            <b-card
+                text-variant="white"
+                class="pb-4">
+              <b-card-title
+                  class="text-center text-light"
+              >
+                Edit Fines
+              </b-card-title>
+              <b-icon icon="cash-stack"
+                      class="h1"
+                      style="width: 120px; height: 120px">
+              </b-icon>
+            </b-card>
+          </router-link>
+        </b-col>
+        <b-col cols="3">
+          <router-link to="/edit_reservations" class="router-link-dashboard">
+            <b-card
+                text-variant="white"
+                class="pb-4"
+            >
+              <b-card-title
+                  class="text-center text-light"
+              >
+                Edit Reservations
+              </b-card-title>
+              <b-icon icon="journal-bookmark-fill"
+                      class="h1"
+                      style="width: 120px; height: 120px">
+              </b-icon>
+            </b-card>
+          </router-link>
+        </b-col>
+        <b-col cols="3">
+          <router-link to="/edit_readers" class="router-link-dashboard">
+            <b-card
+                text-variant="white"
+                class="pb-4"
+            >
+              <b-card-title
+                  class="text-center text-light"
+              >
+                Edit Readers
+              </b-card-title>
+              <b-icon icon="person"
+                      class="h1"
+                      style="width: 120px; height: 120px">
+              </b-icon>
+            </b-card>
+          </router-link>
+        </b-col>
       </b-row>
     </b-container>
 
@@ -106,5 +135,20 @@ export default {
 </script>
 
 <style scoped>
+a{
+  text-decoration: none;
+}
 
+.card{
+  border-radius: 0.25rem;
+  background: #24433e;
+  box-shadow: 0 6px 10px rgba(0,0,0,.08), 0 0 6px rgba(0,0,0,.05);
+  transition: .3s transform cubic-bezier(.155,1.105,.295,1.12),.3s box-shadow,.3s -webkit-transform cubic-bezier(.155,1.105,.295,1.12);
+  cursor: pointer;
+}
+
+.card:hover{
+  transform: scale(1.05);
+  box-shadow: 0 10px 20px rgba(0,0,0,.12), 0 4px 8px rgba(0,0,0,.06);
+}
 </style>
